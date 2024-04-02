bc23a204c05d0cb39d9b2e9244cfbb2d6fc900a7
GH <130484247+vvzsf@users.noreply.github.com>
2024-04-02 22:01:08 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,6 +1,6 @@
 class script(object):
-    START_TXT = """**Hello {},**
-**Myself <a href=https://t.me/{}>{}</a>,\n\nI Am PowerFul Autofilter + ManualFilter**\n\n**You Can Use Only This Group : /nhttps://t.me/+Dt6DotXT4Uw2OTk1/n**"""
+    START_TXT = """ʜᴇ'ʟʟᴏ {}, 
+    ᴍʏ ᴍʏsᴇʟғ <a href=https://t.me/{}>{}</a>,\n\nɪ ᴀᴍ ᴘᴏᴡᴇʀғᴜʟ ғɪʟᴛᴇʀ ɪ ᴄᴀɴ sʜᴀʀᴇ ᴍᴏᴠɪᴇs ᴀɴᴅ sᴇʀɪᴇs\n\nʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴏɴʟʏ ᴛʜɪs ɢʀᴏᴜᴘ :-\nhttps://t.me/+Dt6DotXT4Uw2OTk1\n\nᴀᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴛᴏ sᴇᴇ ᴛʜᴇ ᴍᴀɢɪᴄ 🪄"""
     LZTHMB_TEXT = """Hello {},
 Glad to see you here. It seems that you really love <a href=https://t.me/PIFOTT >RareMovies's</a> work.\n\n<b>Thumbnail extracting</b> feature will be available soon, please join <a href=https://t.me/PanindiaFilmZ>Main Channel</a> and stay tuned for next <a href=https://t.me/PIFDeals>Deals</a>.\n\n  🐞 Report Bug here: <a href=http://t.me/PanIndia_Flimz_Admin_bot>Developer</a>
     """
