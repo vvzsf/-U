234828ff0e1e9ed7a829b17b03cc4b76ad466f0e
GH <130484247+vvzsf@users.noreply.github.com>
2024-04-02 18:08:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -39,9 +39,9 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "New")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'New')
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://vcdeals24:Saahus12@cluster0.yohzzpw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Naaa2")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Loveda_files')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
